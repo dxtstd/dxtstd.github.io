7024ce96be4a641fe76033081173cdc73d8ebbea
dxtstd <denta.ft@dxtstd.my.id>
2025-11-18 08:59:55 +0800

    update files

--- src/deprecated/legacy.tsx
+++ src/deprecated/legacy.tsx
@@ -1,87 +1,94 @@
-import { Component, useState } from "react";
+import { Component, useState, type ReactElement } from "react";
 import "../style.css";
 
-class TextoAnimation extends Component {
+class TextAnimation extends (Component) {
   public list: string[] = [
-    "dxtstd",
-    "newbie programmer",
-    "anime"
+    "dxtstd", "newbie programmer"
   ]
-  public state: { index: number, text: string } = {
+
+  public state: { index: number, text: string, isRunning: boolean } = {
     index: 0,
-    text: ""
+    text: "",
+    isRunning: false
   }
 
-  async sleep(ms: number) {
-    return new Promise((resolve, reject) => setTimeout(resolve, ms))
+  async sleep(ms: number): Promise<undefined> {
+    return new Promise((resolve) => setTimeout(resolve, ms))
   }
 
-  async loop() {
+  async loop(): Promise<undefined> {
+    if (this.state.isRunning) return void 0;
+
+    this.state.isRunning = true;
+
     const selectedText = this.list[this.state.index];
     let selectedTextLength = selectedText.length;
 
     for (const letter of this.list[this.state.index].split("")) {
       this.setState({ text: this.state.text + letter });
-      await this.sleep(150)
-      selectedTextLength --
+      await this.sleep(150);
+      selectedTextLength--;
     }
 
     await this.sleep(5000)
 
     while (selectedTextLength < (selectedText.length + 1)) {
-      selectedTextLength ++
-      this.setState({ text: (this.state.text).split("").reverse().splice(1).reverse().join("") })
-      await this.sleep(75)
+      selectedTextLength++;
+      this.setState({ text: (this.state.text).split("").reverse().splice(1).reverse().join("") });
+      await this.sleep(75);
     }
 
     if (this.state.index >= (this.list.length - 1)) {
-      this.setState({ index: 0 })
+      this.setState({ index: 0 });
     } else {
-      this.setState({ index: this.state.index + 1 })
+      this.setState({ index: this.state.index + 1 });
     }
 
+    await this.sleep(100)
+
+    if (this.state.text.length == 0) this.state.isRunning = false;
     this.loop()
   }
 
-  componentDidMount() {
-    this.loop()
+  componentDidMount(): undefined {
+    this.loop();
   }
 
-  render() {
+  render(): ReactElement {
     return (
       <>
-      <p>I'm {this.state.text}</p>
+        <p>I'm {this.state.text}</p>
       </>
     )
   }
 }
 
-export default function App () {
+export default function App(): ReactElement {
   const [state, setState] = useState<string>("test_1")
   return (
     <div className="bg-slate-900">
       <div className="min-h-screen relative flex flex-col justify-center gap-[25px]">
         <div className="relative bg-slate-800 px-6 pb-8 pt-10 shadow-2xl ring-1 ring-slate-500/50 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
           <h1 className="font-bold underline">Hello World</h1>
-          <TextoAnimation></TextoAnimation>
+          <TextAnimation />
         </div>
         <div className="relative bg-slate-800 px-6 pb-8 pt-10 shadow-2xl ring-1 ring-slate-500/50 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
           <ul className="flex justify-center  gap-[25px]">
-          {([{id: "test_1", display: "test"}, {id: "test_2", display: "test"}, {id: "test_3", display: "test"}]).map((value, index) => (
-            <li 
-              key={value.id}
-              className={(() => {
-                let def_class = ["h-[40px]", "w-[80px]", "text-center", "items-center ", "rounded-[50px]",  "hover:scale-[1.1]", "duration-[0.2s]"]
-                if (state === value.id) {
-                  def_class.push("bg-[#3d81ff]", "hover:bg-[#3d81ff]")
-                } else def_class.push("bg-[#a7aebb]", "hover:bg-[#6fbaff]")
+            {([{ id: "test_1", display: "test" }, { id: "test_2", display: "test" }, { id: "test_3", display: "test" }]).map((value) => (
+              <li
+                key={value.id}
+                className={(() => {
+                  let def_class = ["h-[40px]", "w-[80px]", "text-center", "items-center ", "rounded-[50px]", "hover:scale-[1.1]", "duration-[0.2s]"]
+                  if (state === value.id) {
+                    def_class.push("bg-[#3d81ff]", "hover:bg-[#3d81ff]")
+                  } else def_class.push("bg-[#a7aebb]", "hover:bg-[#6fbaff]")
 
-                return def_class.join(" ")
-              })()}
-              onClick={() => { setState(value.id);console.log(state) }}
-            >
-              {value.display}
-            </li>
+                  return def_class.join(" ")
+                })()}
+                onClick={() => { setState(value.id); console.log(state) }}
+              >
+                {value.display}
+              </li>
             ))}
           </ul>
         </div>
--- src/portofolio.tsx
+++ src/portofolio.tsx
@@ -1,12 +1,75 @@
 import "./portofolio.css"
 import "./style.css"
 
-import img_of_my_boy from "./assets/myboy.png" 
+import { Component, type ReactElement } from "react"
+import img_of_my_boy from "./assets/myboy.webp"
 
 const introduction: string = "Someone who wants to create technology that is useful for the future. He does programming, editing and designing to find an identity. Learning new things is a challenge for him, he does everything to improve his skills."
 //                            || "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas auctor dignissim ipsum, eu eleifend metus. Mauris fermentum eros eget eros pharetra bibendum nec ut neque. Nullam euismod nunc est, ut facilisis urna mattis id. Duis vehicula fringilla ligula. Pellentesque ullamcorper feugiat magna vitae commodo." 
 
-export default function MyPortofolio() {
+class TextAnimation extends (Component) {
+  public list: string[] = [
+    "Newbie Programmer", "Network Engineer", "IT Support"
+  ]
+
+  public state: { index: number, text: string, isRunning: boolean } = {
+    index: 0,
+    text: "",
+    isRunning: false
+  }
+
+  async sleep(ms: number): Promise<undefined> {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
+
+  async loop(): Promise<undefined> {
+    if (this.state.isRunning) return void 0;
+
+    this.state.isRunning = true;
+
+    const selectedText = this.list[this.state.index];
+    let selectedTextLength = selectedText.length;
+
+    for (const letter of this.list[this.state.index].split("")) {
+      this.setState({ text: this.state.text + letter });
+      await this.sleep(75);
+      selectedTextLength--;
+    }
+
+    await this.sleep(5000)
+
+    while (selectedTextLength < (selectedText.length + 1)) {
+      selectedTextLength++;
+      this.setState({ text: (this.state.text).split("").reverse().splice(1).reverse().join("") });
+      await this.sleep(50);
+    }
+
+    if (this.state.index >= (this.list.length - 1)) {
+      this.setState({ index: 0 });
+    } else {
+      this.setState({ index: this.state.index + 1 });
+    }
+
+    await this.sleep(50)
+
+    if (this.state.text.length == 0) this.state.isRunning = false;
+    this.loop()
+  }
+
+  componentDidMount(): undefined {
+    this.loop();
+  }
+
+  render(): ReactElement {
+    return (
+      <>
+        {this.state.text.length == 0 ? "loading..." : `${this.state.text}`}
+      </>
+    )
+  }
+}
+
+export default function MyPortofolio(): ReactElement {
   return (
     <>
       <div className="m-auto">
@@ -14,7 +77,7 @@ export default function MyPortofolio() {
           <div className="p-[25px] m-auto">
             <h1>dxtstd</h1>
             <hr className="line"></hr>
-            <p className="text-[14px] text-[#cfcfcf]">newbie programmer</p>
+            <p className="title text-[14px] text-[#cfcfcf]"><TextAnimation /></p>
             <article className="w-[250px] sm:w-[400px] h-[150px]">
               <p className="text-justify">{introduction}</p>
             </article>
@@ -24,6 +87,87 @@ export default function MyPortofolio() {
               <img alt="this_is_me" src={(() => img_of_my_boy)()}></img>
             </div>
           </div>
+        </section>
+        <div className="flex flex-col xl:flex-row h-dvh max-h-dvh justify-around w-dvw p-16">
+          <section id="experience">
+            <div className="px-16">
+              <div>
+                <h1 className="text-center m-auto">Experience</h1>
+                <hr className="line items-center m-auto"></hr>
+              </div>
+              <div className="flex flex-col gap-2 py-8">
+                <div className="experience" id="exp_telkom_akses">
+                  <h2>Internship</h2>
+                  <hr className="line"></hr>
+                  <div className="place flex flex-row gap-1">
+                    <p>at PT. Telkom Akses</p>
+                    <p> · </p>
+                    <p>July 2024 - November 2024</p>
+                  </div>
+                  <div>
+                    <article>
+                      <p>Description: Saya disini bekerja sebagai anu</p>
+                    </article>
+                  </div>
+                </div>
+                <div className="experience" id="exp_imphnen">
+                  <h2>Moderator</h2>
+                  <hr className="line"></hr>
+                  <div className="place flex flex-row gap-1">
+                    <p>at IMPHNEN</p>
+                    <p> · </p>
+                    <p>February 2024 - Present</p>
+                  </div>
+                  <div>
+                    <article>
+                      <p>Description: Saya disini bekerja sebagai anu</p>
+                    </article>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section id="education">
+            <div className="px-16">
+              <div>
+                <h1 className="text-center m-auto">Education</h1>
+                <hr className="line items-center m-auto"></hr>
+              </div>
+              <div className="flex flex-col gap-2 py-8">
+                <div className="education" id="edu_smk">
+                  <h2>Vocational High School</h2>
+                  <hr className="line"></hr>
+                  <div className="place flex flex-row gap-1">
+                    <p>at SMKS Handayani Sungguminasa</p>
+                    <p> · </p>
+                    <p>July 2022 - April 2025</p>
+                  </div>
+                  <div>
+                    <article>
+                      <p>Description: Saya disini bekerja sebagai anu</p>
+                    </article>
+                  </div>
+                </div>
+                <div className="education" id="edu_collage">
+                  <h2>Bachelor (Collage)</h2>
+                  <hr className="line"></hr>
+                  <div className="place flex flex-row gap-1">
+                    <p>at Universitas Negeri Manado</p>
+                    <p> · </p>
+                    <p>August 2025 - Present</p>
+                  </div>
+                  <div>
+                    <article>
+                      <p>Description: Saya disini bekerja sebagai anu</p>
+                    </article>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+        <section id="project">
+
         </section>
       </div>
     </>
